bb5fa73d73faed3a43639c17431dea1a5231ff70
Sumanshu <sumanshuborkar569@gmail.com>
2023-04-19 16:15:22 +0530

    Added the Landing Page

--- ClientSide/src/Components/Navigation.js
+++ ClientSide/src/Components/Navigation.js
@@ -2,15 +2,18 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import Logo from "./images/Logo.png"
+import "./Style/Navig.css"
 
 const Navigation = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
+      <Navbar className='navbar'>
+        <Container >
+          <Navbar.Brand href="#home" >
+          <img src={Logo} alt="not available" className='Logo' />
+          </Navbar.Brand>
+          <Nav className='navholder' >
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="features" >Features</Nav.Link>
             <Nav.Link href="/pricing">Pricing</Nav.Link>
--- ClientSide/src/User/UserSign.js
+++ ClientSide/src/User/UserSign.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./../Components/Style/Form.css"
 
 function UserSign() {
   return (
     <>
-    <h2>User Signup</h2>
-    <Form className="container d-flex justify-content-center flex-column" style={{ height: '500px', width: '300px' }}>
+    <Form className="container d-flex justify-content-center flex-column " id='form'>
+    <h2 className='fom'>User Signup</h2>
   <Form.Group className="mb-3" controlId="formBasicName">
     <Form.Label>Name</Form.Label>
     <Form.Control type="text" placeholder="Enter your name" />
@@ -16,9 +17,6 @@ function UserSign() {
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicContact">
