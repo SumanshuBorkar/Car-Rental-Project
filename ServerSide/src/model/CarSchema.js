85ee91984c8e9102520f5be1fc052aa5694f3d62
jadhav-dipali <jadhavdipali924@gmail.com>
2023-04-20 11:11:23 +0530

    Backend add car details are added

---
+++ ServerSide/src/model/CarSchema.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+
+const carSchema = mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    type:{
+        type:String,
+        require:true
+    },
+    model:{
+        type:String,
+        require:true
+    },
+    milage:{
+        type:String,
+        require:true
+    },
+    image:{
+        type:String,
+    },
+    availabalFrom:{
+       type:String
+    },
+    availabalTill:{
+        type:String
+    },
+    perKm:{
+        type:String
+    },
+    description:{
+        type:String
+    },
+    carDetails:{
+        type:String
+    },
+    Details:{
+        type:String
+    }
+})
+
+const carDetails = new mongoose.model("cardata" , carSchema)
+
+module.exports=carDetails
