17347e32607ea72b74136ce32b85547f6bb39ef8
suman-kushwaha <suman207kumari@gmail.com>
2023-04-25 20:14:34 +0530

    some more edition to styling

--- ClientSide/src/Components/Cards.js
+++ ClientSide/src/Components/Cards.js
@@ -8,50 +8,52 @@ export default function Cards() {
 
   let [data, setdata] = useState([])
   useEffect(() => {
-    fetch("http://localhost:5000/cars/",{
-      headers:{
-        "authorization":JSON.parse(localStorage.getItem("token-user"))
-    }
+    fetch("http://localhost:5000/cars/", {
+      headers: {
+        "authorization": JSON.parse(localStorage.getItem("token-user"))
+      }
     })
       .then(res => res.json())
       .then(res => setdata(res));
   }, [])
-  
-  const[bookData,setbookData]=useState([])
+
+  const [bookData, setbookData] = useState([])
   console.log(bookData);
 
   return <>
-    <div id="carCard">
-      {
-        data.map((d, i) => {
-          return <div key={i}>
-            <Card className='card'>
+    <div id="mainParents">
+      <div id="carCard">
+        {
+          data.map((d, i) => {
+            return <div key={i}>
+              <Card className='card'>
 
-              <div className="img">
-                <Card.Img src={`http://localhost:5000/cars/${d.image}`}/>
-              </div>
-              <div id="cardBodys">
-                <Card.Body>
-                  <div id="seater">
-                    <Card.Text id="person" >{d.name}</Card.Text>
-                  </div>
-
-                  <div id="name-container">
-                    <Card.Text id="name" >6 persons</Card.Text>
-                    <Card.Text id="milage" >{d.milage}</Card.Text>
-                  </div>
-                </Card.Body>
-
-                <div id="booking">
-                  <Card.Link  id="fare">Fare Details</Card.Link>
-                  <Card.Link href="/payment" id="book" onClick={()=>setbookData({d})} >Book Now</Card.Link>
+                <div className="img">
+                  <Card.Img src={`http://localhost:5000/cars/${d.image}`} />
                 </div>
+                <div id="cardBodys">
+                  <Card.Body>
+                    <div id="seater">
+                      <Card.Text id="person" >{d.name}</Card.Text>
+                    </div>
 
-              </div>
-            </Card>
-          </div>
-        })
-      }
+                    <div id="name-container">
+                      <Card.Text id="name" >6 persons</Card.Text>
+                      <Card.Text id="milage" >{d.milage}</Card.Text>
+                    </div>
+                  </Card.Body>
+
+                  <div id="booking">
+                    <Card.Link id="fare">Fare Details</Card.Link>
+                    <Card.Link href="/payment" id="book" onClick={() => setbookData({ d })} >Book Now</Card.Link>
+                  </div>
+
+                </div>
+              </Card>
+            </div>
+          })
+        }
+      </div>
     </div>
   </>
 }
